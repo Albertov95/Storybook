e89aed849139e4039ba6d2ef37694c9a3961cf70
Albertov95 <60914697+Albertov95@users.noreply.github.com>
2021-01-29 17:10:12 +0300

    Two points

--- trainee-test-1-master/src/components/base/Image.tsx
+++ trainee-test-1-master/src/components/base/Image.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const ImageBlock = styled.div`
+	background: url(${props => props.src});
+	width: ${props => props.width}px;
+	height: ${props => props.height}px;
+`;
 
 export default (props) => {
 	return (
-		<img
-			alt=""
-			{...props}
-		/>
+		<ImageBlock {...props}/>
 	);
 };
--- trainee-test-1-master/src/components/base/SmokeText.tsx
+++ trainee-test-1-master/src/components/base/SmokeText.tsx
@@ -1,5 +1,22 @@
 import * as React from "react";
+import styled from 'styled-components';
 
 export default ({children}) => {
-	return children;
+	// const leftPart = children.slice(0, children.length / 2);
+	// const rightPart = children.slice(children.length / 2, children.length);
+	// const maskedString = leftPart + "&#8203;" + rightPart;
+	// return <span dangerouslySetInnerHTML={{__html: maskedString}} />
+
+	const UnicornAfter = styled.span`
+		&:after {
+			content: "${children}";
+			display: block;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+		}
+	`;
+
+	return <UnicornAfter />
 };
